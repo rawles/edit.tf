2ff8b56890885eaf45e8d4270701b8870142ffb5
Simon Rawles <rawles@rawles.net>
2016-11-22 23:33:45 +0000

    Double height on row 23 or 24 is not allowed.

--- teletext-editor.js
+++ teletext-editor.js
@@ -1402,18 +1402,17 @@ var hide_status_bar = function() {
 }
 
 var compliance_level = function() { 
+	// Bright red means that the frame can't exist on a broadcast system.
+        if ( fs[23] == 1 || fs[24] > 0 ) { return 0; } 
 	for (var c = 0; c <= 7; c++) { if ( cc[0][c] != 32 ) return 0; }
+
+	// Dull red means that the user has overwritten the header line.
 	for (var c = 8; c <= 39; c++) { if ( cc[0][c] != 32 ) return 1; }
+
+	// Yellow means that row 24, reserved for Fastext, has been overwritten.
 	for (var c = 0; c <= 39; c++) { if ( cc[24][c] != 32 ) return 2; }
-        // The last row might have double height on it.
-        if ( fs[23] == 1 ) {
-                // do any of the characters descend into row 24?
-                for (var c = 0; c <= 39; c++ ) {
-                        if ( nd[23][c] == 1 && cc[23][c] != 32 ) {
-                                return 2;
-                        }
-                }
-        }
+
+	// Green means everything is groovy.
 	return 3;
 }
 
